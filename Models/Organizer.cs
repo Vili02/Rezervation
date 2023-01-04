9c938bf32605ab275b9b584ad69c79d5a1db7cff
Velislava <viliasparuhova02@gmail.com>
2023-01-04 17:42:00 +0200

    Add Organizer.cs

---
+++ Models/Organizer.cs
@@ -0,0 +1,14 @@
+﻿namespace Rezervation.Models
+{
+    public class Organizer
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phonenumber { get; set; }
+        public string Info { get; set; }
+        public ICollection<Trip> Trips { get; set; }
+    }
+}
+

dcd09f10aedb37d733d9e5c08e9813da05e54de2
Velislava <viliasparuhova02@gmail.com>
2023-01-06 18:11:37 +0200

    Add ApiContext

---
+++ Data/ApiContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rezervation.Models;
+
+
+
+namespace Rezervation.Data
+{
+    public class ApiContext : DbContext
+    {
+        public DbSet<Trip>Trips { get; set; }
+        public ApiContext(DbContextOptions<ApiContext>options)
+            :base
+
+
+
+
+    }
+}
